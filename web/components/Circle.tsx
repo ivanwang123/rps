a15f37821072a569f0bdb058e363b6fdd085fc3a
ivanwang123 <ivanwang02@gmail.com>
2022-01-10 20:15:11 -0500

    Add link to circle

--- web/components/Circle.tsx
+++ web/components/Circle.tsx
@@ -1,15 +1,16 @@
+import Link from "next/link";
 import React from "react";
 
 export function Circle() {
   return (
-    <article className="mx-5 mb-10">
-      <button type="button" className="flex flex-col items-center group">
+    <Link href="/play">
+      <a className="flex flex-col items-center mx-5 mb-10 group">
         <div className="w-28 h-28 bg-orange-100 border-4 border-black rounded-full mb-1"></div>
         <p className="font-bold underline-offset-2 group-hover:underline">
           Product Hunt
         </p>
         <p className="condensed-font text-sm">23 PLAYERS</p>
-      </button>
-    </article>
+      </a>
+    </Link>
   );
 }
